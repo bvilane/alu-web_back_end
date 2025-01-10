f245cfdfaf5a7bac5831e43ddc89bf020c1181aa
bvilane <b.vilane@alustudent.com>
2025-01-10 20:08:04 +0200

    Updated

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 '''Type Checking'''
-from typing import Sequence, List, Union
+from typing import Tuple, List
 
 
-def zoom_array(lst: Sequence[Union[int, float]], factor: int = 2) -> List[Union[int, float]]:
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     """Function to zoom in an array by duplicating its elements."""
-    zoomed_in: List[Union[int, float]] = [
+    zoomed_in: List[int] = [
         item for item in lst
         for _ in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
