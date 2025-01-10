bec729962d3125c4d51349c7a0bba53b171aa35e
bvilane <b.vilane@alustudent.com>
2025-01-10 20:04:35 +0200

    Type Checking

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''Type Checking'''
+from typing import List, Tuple, Union
+
+
+def zoom_array(lst: List[Union[int, float]], factor: int = 2) -> List[Union[int, float]]:
+    """Function to zoom in an array by duplicating its elements."""
+    zoomed_in: List[Union[int, float]] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
