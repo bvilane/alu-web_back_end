ef2dc08a435398639bccc28268a604f87240cdd1
bvilane <b.vilane@alustudent.com>
2025-01-10 20:09:42 +0200

    New Update

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -3,9 +3,9 @@
 from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Function to zoom in an array by duplicating its elements."""
-    zoomed_in: List[int] = [
+    zoomed_in = [
         item for item in lst
         for _ in range(factor)
     ]
@@ -17,3 +17,4 @@ array = (12, 72, 91)
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
+
